97226a68492bd7facb1c84fd25fcf49a5a1b3a7a
DorianP <albnet@gmail.com>
2015-09-27 19:47:03 -0400

    Reorder steps in predict

--- linda_predict.R
+++ linda_predict.R
@@ -1,28 +1,20 @@
-# load other functions
-scriptdir = dirname(sys.frame(1)$ofile)
-source(file.path(scriptdir, 'getLesionFeatures.R'), echo=F)
-source(file.path(scriptdir, 'mrvnrfs_chunks.R'), echo=F)
-
-
-# install necessary packages and load them
+# check for necessary packages and load them
 if (! is.element("ANTsR", installed.packages()[,1])) {
   stop("Required ANTsR package cannot be found.
         Automated installation not possible.
        See http://stnava.github.io/ANTsR for installation instructions")
+} else {
+  library(ANTsR)
 }
 if (! is.element("randomForest", installed.packages()[,1])) {
   print("Installing missing `randomForest` package")
   install.packages("randomForest")
+} else { 
+  library(randomForest)
 }
 
 
-# load template files
-cat(paste(format(Sys.time(), "%H:%M") , "Loading template... \n"))
-temp = antsImageRead(file.path(scriptdir,'pennTemplate','template.nii.gz'))
-tempbrain = antsImageRead(file.path(scriptdir,'pennTemplate','templateBrain.nii.gz'))
-tempmask = antsImageRead(file.path(scriptdir,'pennTemplate','templateBrainMask.nii.gz'))
-                                                   
-
+# parse arguments for command line use
 args=commandArgs(TRUE)
 for(i in 1:length(args)){
   eval(parse(text=args[i]))
@@ -38,6 +30,7 @@ if (! exists('t1')) {
   }
 }
 
+                                                  
 # check this is a nifti extension
 if (length(grep(".nii$", t1)) == 0 & length(grep(".nii.gz$", t1)) == 0) {
   stop(
@@ -46,15 +39,31 @@ if (length(grep(".nii$", t1)) == 0 & length(grep(".nii.gz$", t1)) == 0) {
 }
 
 
+# load the file
+cat(paste(format(Sys.time(), "%H:%M") , 'Loading file:', basename(t1), "\n"))
+subimg = antsImageRead(t1)
+submask = subimg*0 + 1
+
+
 # create linda folder
 lindadir = file.path(dirname(t1), 'linda')
 cat(paste(format(Sys.time(), "%H:%M") , 'Creating folder:', lindadir, "\n"))
 dir.create(lindadir, showWarnings = F, recursive = T)
 
-# load the file
-cat(paste(format(Sys.time(), "%H:%M") , 'Loading file:', basename(t1), "\n"))
-subimg = antsImageRead(t1)
-submask = subimg*0 + 1
+
+
+# load other functions
+scriptdir = dirname(sys.frame(1)$ofile)
+source(file.path(scriptdir, 'getLesionFeatures.R'), echo=F)
+source(file.path(scriptdir, 'mrvnrfs_chunks.R'), echo=F)
+
+
+# load template files
+cat(paste(format(Sys.time(), "%H:%M") , "Loading template... \n"))
+temp = antsImageRead(file.path(scriptdir,'pennTemplate','template.nii.gz'))
+tempbrain = antsImageRead(file.path(scriptdir,'pennTemplate','templateBrain.nii.gz'))
+tempmask = antsImageRead(file.path(scriptdir,'pennTemplate','templateBrainMask.nii.gz'))
+
 
 
 # two rounds of N4-BrainExtract to skull strip
